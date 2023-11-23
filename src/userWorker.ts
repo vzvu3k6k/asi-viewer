3a2a1637c45d65ef03e219bbc672054641a91ad6
vzvu3k6k <vzvu3k6k@gmail.com>
2023-11-23 23:35:08 +0900

    Remove needless any

--- src/userWorker.ts
+++ src/userWorker.ts
@@ -3,7 +3,7 @@ import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 self.MonacoEnvironment = {
-  getWorker(_: any, label: string) {
+  getWorker(_, label: string) {
     if (label === "typescript" || label === "javascript") {
       return new tsWorker();
     }
